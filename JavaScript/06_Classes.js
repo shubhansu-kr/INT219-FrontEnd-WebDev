f9aaa19dfeb5b447b7f36750092c35704e42eed2
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-16 11:33:31 +0530

    Classes
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ JavaScript/06_Classes.js
@@ -0,0 +1,16 @@
+class Fruit {
+    constructor(fName, price) {
+        this.fName = fName;
+        this.price = price;
+    }
+}
+
+let apple = new Fruit("apple", 210);
+let banana = new Fruit("banana", 60);
+
+// Constructor: 
+
+// 1. DefaultConstructor
+// 2. ZeroArgumentConstructor
+// 3. CopyConstructor
+// 4. 
