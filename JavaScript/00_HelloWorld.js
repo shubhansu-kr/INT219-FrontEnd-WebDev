a19f2acb1ad77afd531e6166640ce205ee642b1e
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-16 10:23:04 +0530

    HelloWorld - JavaScript
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ JavaScript/00_HelloWorld.js
@@ -0,0 +1,25 @@
+// Who developed JavaScript ? 
+
+console.log("Hello World"); 
+// Pro JavaScript Developer.
+
+// JavaScript is a dynamic computer programming language. It is lightweight 
+// and most commonly used as a part of web pages, whose implementations allow 
+// client-side script to interact with the user and make dynamic pages. It is 
+// an interpreted programming language with object-oriented capabilities.
+
+// Client-Side JavaScript
+// Client-side JavaScript is the most common form of the language. 
+// The script should be included in or referenced by an HTML document 
+// for the code to be interpreted by the browser.
+
+// It means that a web page need not be a static HTML, but can include 
+// programs that interact with the user, control the browser, and dynamically 
+// create HTML content.
+
+// Single line comment 
+
+/* 
+    Multi Line Comment
+*/
+
