0f7a9d73b2d0c3fc7b79175dd301b4a00367ef0e
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-16 10:56:58 +0530

    Functions
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ JavaScript/02_Functions.js
@@ -0,0 +1,17 @@
+// Functions: JavaScript
+
+let a = function name(...ab) {
+    console.log(ab);
+}
+
+a();
+
+const greet = (user) => {
+    console.log(`Welcome ${user}`);
+    return 21;
+}
+
+let user = "Shubh";
+
+greet(user);
+console.log(greet(user));
