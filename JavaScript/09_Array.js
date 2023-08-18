27830cd5a52a929b1b5a8673152baf2733b1d026
Shubhansu kumar <shubhansu2021@gmail.com>
2023-08-18 10:52:08 +0530

    Arrays in javascript
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ JavaScript/09_Array.js
@@ -0,0 +1,55 @@
+// Arrays in JS 
+
+const arr = [];
+const arr1 = [21, 13, 15];
+const arr2 = new Array(5); // array of size 5;
+
+const arr3 = new Array("Hey", "hey", "HEY", "hEY");
+
+for (let index = 0; index < arr3.length; index++) {
+    const element = arr3[index];
+    
+    console.log(element);
+}
+
+arr3.forEach(x => {
+    console.log(x);
+});
+
+for (const itr of arr3) {
+    console.log(itr);
+}
+
+// Mapping to create a new array.
+const arr4 = arr3.map((x)=>{return (x + " Apple");});
+console.log(arr4);
+
+const arr5 = arr3.map((x)=>(x + " lol"));
+console.log(arr5);
+
+
+const arr6 = arr1.toString();
+// arr6 is not an array.
+console.log(typeof(arr6), arr6);
+
+// join() : Concatinates all the element in a string and returns it.
+console.log(arr5.join());
+
+console.log(arr5.shift()); // removes the first element
+console.log(arr5.unshift(231)); // add the element at front.
+
+console.log(arr5);
+// delete(arr1);
+console.log(arr1);
+
+const splicedElements = arr5.splice(2, 3, "newly", "inserted", "elements");
+console.log(splicedElements, arr5);
+
+const a = arr5.slice(1, 3);
+console.log(typeof(a), " : ", a);
+
+arr5.sort();
+console.log(arr5);
+
+arr5.reverse();
+console.log(arr5);
